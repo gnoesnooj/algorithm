f183e1e2558882f15ab198c99916842d9ab42fde
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2022-07-09 15:53:49 +0800

    Create 튜플.java

---
+++ code/튜플.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution{
+    public int[] solution(String s){
+        Map<Integer, String[]> map = new HashMap<>();
+        Set<String> set =new HashSet<>();
+
+        String str = s.substring(2,s.length()-2);
+        str = str.replaceAll("},\\{", "/");
+        String [] tuples = str.split("/");
+
+        int [] answer = new int[tuples.length];
+
+        for(int i=0; i< tuples.length; i++){
+            String [] parseTuples = tuples[i].split(",");
+            map.put(parseTuples.length, parseTuples);
+        }
+
+        for(int i=1; i<=map.size(); i++){
+            String [] tmp = map.get(i);
+            for(String tmpString : tmp){
+                if(set.contains(tmpString)){
+                    continue;
+                } else {
+                    answer[i-1] = Integer.parseInt(tmpString);
+                    set.add(tmpString);
+                }
+            }
+        }
+        return answer;
+    }
+}
