8c502f880dab4752996952bfc83e1faca9e7ae4e
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2023-02-28 22:29:38 +0800

    Update 피로도.java

--- code/Lv2/피로도.java
+++ code/Lv2/피로도.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int answer = 0;
-    public boolean[] visited;
-
+    int answer = 0;
+    boolean visited[] ;
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        dfs(0, k, dungeons);
+        dfs(0,dungeons, k);
         return answer;
     }
-
-    public void dfs(int depth, int k, int[][] dungeons) {
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && dungeons[i][0] <= k) {
-                visited[i] = true;
-                dfs(depth + 1, k - dungeons[i][1], dungeons);
-                visited[i] = false;
+    
+    public void dfs(int depth, int [][] dungeons, int k){
+        for(int i=0; i<dungeons.length; i++){
+            if(!visited[i] && k >= dungeons[i][0]){ // 방문하지 않았고, 던전 최소 피로도 보다 피로도가 높을 경우
+                visited[i] = true; // 방문했다고 해주고
+                dfs(depth + 1, dungeons, k- dungeons[i][1]); // 방문한 던전+1, 소모한 피로도를 빼준 값을 넣어줘서 다음 dfs 진행
+                visited[i] = false; // 다른 분기이므로 false로 바꿔준다
             }
         }
-        answer = Math.max(answer, depth);
+        answer = Math.max(depth, answer);
     }
 }
