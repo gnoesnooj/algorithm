1a79934acd6a3d98f690b2bdb82aff9ef6e67f85
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2023-10-12 17:00:34 +0900

    부분집합

---
+++ SWEA/D4/D4_1486_장훈이의높은선반_예준성.java
@@ -0,0 +1,68 @@
+package lgo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 20,484 kb 메모리
+ * 175 ms 실행시간
+ */
+public class Solution {
+
+	static int N, B, answer;
+	static int people [];
+	static boolean [] selected;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int tc = Integer.parseInt(br.readLine());
+
+		String[] inputs;
+		for (int T = 1; T <= tc; T++) {
+			inputs = br.readLine().split(" ");
+
+			N = Integer.parseInt(inputs[0]);
+			B = Integer.parseInt(inputs[1]);
+			
+			people = new int [N];
+			selected = new boolean[N];
+			
+			inputs = br.readLine().split(" ");
+			
+			for(int i=0;i<N;i++) {
+				people[i] = Integer.parseInt(inputs[i]);
+			}
+			
+			answer = Integer.MAX_VALUE;
+			find(0);
+			
+			sb.append("#").append(T).append(" ").append(answer).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	private static void find(int cnt) {
+		if(cnt == N) {
+			int sum = 0;
+			for(int i=0; i<N;i++) {
+				if(selected[i]) {
+					sum += people[i];
+					if(sum >= B) {
+						answer = Math.min(sum-B, answer);
+					}
+				}
+			}
+		} else {
+			selected[cnt] = true;
+			find(cnt+1);
+			selected[cnt] = false;
+			find(cnt+1);
+		}
+	}
+}
