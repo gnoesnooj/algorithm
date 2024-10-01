96fd6579092e7b35db7a1e841c09f83caea6c780
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2024-10-01 15:47:44 +0800

    조합 완탐

--- 백준/1038 감소하는수.java
+++ 백준/1038 감소하는수.java
@@ -1,1 +1,45 @@
 
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(br.readLine());
+
+        if (input < 11) {
+            System.out.println(input);
+            return;
+        }
+
+        for (int i = 0; i <= 9; i++) {
+            combi(i);
+        }
+
+        if (input >= list.size()) {
+            System.out.println(-1);
+            return;
+        }
+        Collections.sort(list);
+        System.out.println(list.get(input));
+    }
+
+
+    private static void combi(long n) {
+        list.add(n);
+
+        long x = n % 10;
+
+        if (x != 0) {
+            for (long i = x - 1; i >= 0; i--) {
+                long value = n * 10 + i;
+                combi(value);
+            }
+        }
+    }
+
+}
