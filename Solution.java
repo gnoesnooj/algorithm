f3cc889ae524f763ecaac00d4c79440b9b3709f2
306yyy <306yyy@naver.com>
2026-02-10 10:09:00 +0900

    feat: 프로그래머스 이상한 문자 만들기

--- Solution.java
+++ Solution.java
@@ -2,45 +2,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    static String alpha = "abcedfghijklmnopqrstuvwxyz";
-
-    public String solution(long n, String[] bans) {
-        String answer = "";
-        // bans 오름차정렬
-        List<String> words = Arrays.stream(bans).collect(Collectors.toList());
-        words.sort(Comparator.comparing(String::length)
-                .thenComparing(Comparator.naturalOrder()));
-
-        String tmp = findWord(n);
-
-        int deleteCount = 0;
-        for (String s : words) {
-//            System.out.println("S :" +s +"// tmp : "+tmp );
-            if (s.length() < tmp.length() || s.length() == tmp.length() && s.compareTo(tmp) <= 0) {
-                deleteCount += 1;
-                tmp = findWord(n + deleteCount);
-            }
-        }
-//        System.out.println(deleteCount);
-
-        return tmp;
-    }
-
-    public String findWord(long n) {
-        Stack<Integer> index = new Stack<>();
-        long tmp = n;
-        while (true) {
-            index.add((int) tmp % 26);
-            tmp = tmp / 26;
-            if (tmp == 0) {
-                break;
-            }
-        }
-        StringBuilder word = new StringBuilder();
-        while (index.size() > 0) {
-//            System.out.println("num :"+index.peek());
-            word.append(alpha.charAt(index.pop()-1));
-        }
-        return word.toString();
+    public int solution(String[] board) {
+        int answer = 0;
+        return answer;
     }
 }
---
+++ code/구현/이상한_문자_만들기.java
@@ -0,0 +1,31 @@
+package 구현;
+
+class 이상한_문자_만들기 {
+    public String solution(String s) {
+
+        StringBuffer sb = new StringBuffer();
+
+        int cnt =0;
+
+        for(int i=0;i<s.length();i++){
+            if((int)s.charAt(i)==32){
+                cnt=0;
+                sb.append(s.charAt(i));
+            }
+            else{
+                if(cnt % 2 == 0){
+                    String str = Character.toString(s.charAt(i)).toUpperCase();
+                    sb.append(str);
+                }
+                else{
+                    String str = Character.toString(s.charAt(i)).toLowerCase();
+                    sb.append(str);
+                }
+                cnt++;
+            }
+        }
+        String answer = sb.toString();
+
+        return answer;
+    }
+}
