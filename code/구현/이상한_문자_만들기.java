a597fe853bdcceb4823e2b875683f347fd362722
306yyy <306yyy@naver.com>
2026-02-10 10:11:46 +0900

    feat: 프로그래머스 이상한 문자 만들기

--- code/구현/이상한_문자_만들기.java
+++ code/구현/이상한_문자_만들기.java
@@ -5,19 +5,17 @@ class 이상한_문자_만들기 {
 
         StringBuffer sb = new StringBuffer();
 
-        int cnt =0;
+        int cnt = 0;
 
-        for(int i=0;i<s.length();i++){
-            if((int)s.charAt(i)==32){
-                cnt=0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((int) s.charAt(i) == 32) {
+                cnt = 0;
                 sb.append(s.charAt(i));
-            }
-            else{
-                if(cnt % 2 == 0){
+            } else {
+                if (cnt % 2 == 0) {
                     String str = Character.toString(s.charAt(i)).toUpperCase();
                     sb.append(str);
-                }
-                else{
+                } else {
                     String str = Character.toString(s.charAt(i)).toLowerCase();
                     sb.append(str);
                 }
